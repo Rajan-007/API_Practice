1e7ba466387c658560b2e7c9b58d0a926a122c8f
Rajan-007 <aravinthrajan034@gmail.com>
2023-05-05 01:58:03 +0530

    POST Method

--- index.js
+++ index.js
@@ -4,6 +4,24 @@
 // .then((joke=> console.log(joke[0].setup,joke[0].punchline)))
 // .catch((err)=> console.log(err))
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+/* GET /
+         POST  */
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+// POST Method
+
+fetch('https://jsonplaceholder.typicode.com/todos',{
+    method : 'POST',
+    headers : {'content-type' : 'application/json'} , 
+    body : JSON.stringify({
+        UserId : 56,
+        Id : 46,
+        title:'testu',
+        completed:false
+    })
+})
       .then(response => response.json())
       .then(json => console.log(json))
