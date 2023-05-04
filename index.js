fe1d9bcbeacdd0583bb8ac23466c9e09b0fc294e
Rajan-007 <aravinthrajan034@gmail.com>
2023-05-05 01:31:28 +0530

    [placeholder API]

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
-fetch('https://official-joke-api.appspot.com/jokes/programming/random')
-.then((res)=>res.json())
-.then((joke=> console.log(joke[0].setup,joke[0].punchline)))
+// fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+// .then((res)=>res.json())
+// .then((joke=> console.log(joke[0].setup,joke[0].punchline)))
+// .catch((err)=> console.log(err))
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
